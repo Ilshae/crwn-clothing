a28aad7a853f9c7a22bfb72485c5166123eba29d
Ingrid <69380470+Ilshae@users.noreply.github.com>
2021-01-15 11:28:15 +0100

    Initial

---
+++ precache-manifest.7bf984a2c71da474a51f7521bbc5b25b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd5bbeb7ad7f32f6264ed6f062cd8cd",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "0debf4693bd4a0345837",
+    "url": "/crwn-clothing/static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "67dd3e48edc8cef77a54",
+    "url": "/crwn-clothing/static/js/2.53da2f7e.chunk.js"
+  },
+  {
+    "revision": "a8d317973ab0f55ce394",
+    "url": "/crwn-clothing/static/js/3.ba3f7235.chunk.js"
+  },
+  {
+    "revision": "686358670e6ab60b70ad",
+    "url": "/crwn-clothing/static/js/4.742b330b.chunk.js"
+  },
+  {
+    "revision": "537d818c57b28baf2913",
+    "url": "/crwn-clothing/static/js/5.0d7d1188.chunk.js"
+  },
+  {
+    "revision": "35aba796d3fe5d2449aa",
+    "url": "/crwn-clothing/static/js/6.84bce3d1.chunk.js"
+  },
+  {
+    "revision": "33fc0f7fd94c1a461341",
+    "url": "/crwn-clothing/static/js/7.39604ae0.chunk.js"
+  },
+  {
+    "revision": "e95c11f56818c8bac640",
+    "url": "/crwn-clothing/static/js/8.01efb9f2.chunk.js"
+  },
+  {
+    "revision": "f5d1f154cb9877e1b6a4",
+    "url": "/crwn-clothing/static/js/9.10b46a98.chunk.js"
+  },
+  {
+    "revision": "0debf4693bd4a0345837",
+    "url": "/crwn-clothing/static/js/main.3fccdf5f.chunk.js"
+  },
+  {
+    "revision": "c37d646e16d7e0f4e21a",
+    "url": "/crwn-clothing/static/js/runtime~main.b7b81706.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
